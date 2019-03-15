8a5307c161bc995ba8b7985bf5c0249172452d25
simon_wozniak <wozniaks97@gmail.com>
2019-03-16 00:01:33 +0100

    m

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,19 @@ int main() {
     TTPProblem problem;
     std::string problemName("medium_0");
     std::string filename(problemName + ".ttp");
-    int iterations = 1000;
+    int iterations = 100000;
     int popSize = 100;
     int tournamentSize = 5;
-    double mutProb = 0.02;
+    double mutProb = 0.01;
     double crossProb = 0.7;
     problem.initialize(filename, ProfitWeightRatio);
     Logger logger(true, problemName + ".txt");
-//    GeneticAlgorithm ga(popSize, crossProb, mutProb, tournamentSize, &problem, &logger);
-//    ga.run(iterations);
+    GeneticAlgorithm ga(popSize, crossProb, mutProb, tournamentSize, &problem, &logger);
+    ga.run(iterations);
 //    RandomSearch randomSearch(&problem, &logger);
 //    randomSearch.search(1000000);
-    HillClimber hillClimber(&problem, &logger);
-    hillClimber.hillClimb(100);
+//    HillClimber hillClimber(&problem, &logger);
+//    hillClimber.hillClimb(100);
     std::cout << problem.getFitnessFunctionEvaluations();
     logger.close();
 }
